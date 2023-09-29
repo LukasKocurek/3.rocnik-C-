7030facbfc9282e09560375250fed422f57bcdca
LukasKocurek <lukasko.kocurek@gmail.com>
2023-09-29 09:16:20 +0200

    kalkulacka, jednoducha

---
+++ cv1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ahoj ja som virtualny papousek Lora, rad opakujem!");
+Console.WriteLine("Napis nieco, co budem opakovat:");
+
+string vstup;
+vstup=Console.ReadLine();
+
+string vystup;
+vystup = vstup + ",  " + vstup + "!";
+Console.WriteLine(vystup);
+Console.ReadKey();
