10802111a481d046dc62905a08560ec754dee5a6
LukasKocurek <lukasko.kocurek@gmail.com>
2023-09-29 17:12:51 +0200

    kalk

---
+++ kalkulacka/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Vítajte v kalkulačke: ");
+Console.WriteLine("Zadajte prvé číslo: ");
+string s = Console.ReadLine();
+float a = float.Parse(s);
+Console.WriteLine("Zadajte druhé číslo: ");
+string d = Console.ReadLine();
+float b = float.Parse(d);
+float sucet, sucin, rozdiel, podiel;
+sucet = a + b;
+sucin = a * b;
+rozdiel = a - b;
+podiel = a / b;
+Console.WriteLine("Súčet je: "+sucet);
+Console.WriteLine(sucet);
+Console.WriteLine("Rozdiel je: ",rozdiel);
+Console.WriteLine(rozdiel);
+Console.WriteLine("Súčin je: ",sucin);
+Console.WriteLine(sucin);
+Console.WriteLine("Podiel je: ",podiel);
+Console.WriteLine(podiel);
+
+Console.ReadKey();
